cc8105a2331f27546e0bf2a811e9cca25b6768a3
eliizaveta <55347547+eliizaveta@users.noreply.github.com>
2021-04-23 17:30:50 +0300

    cleaning

--- Test/src/test/java/Tests.java
+++ Test/src/test/java/Tests.java
@@ -15,30 +15,11 @@ public class Tests extends BaseTest {
     }
 
     @Test
-<<<<<<< HEAD
+
     public void Test() {
         UserPage userPage = new LoginPage(driver).doLogin(username, password);
         MessagePage msgPage = new MsgTest(driver).testMSG();
     }
-=======
-   public void Test() throws InterruptedException  {
-
-        boolean flag = true;
-        try {
-            UserPage userPage = new LoginPage(driver).doLogin(username, password);
-            Thread.sleep(500);
-            MessagePage msgPage = new MsgTest(driver).testMSG();
-            Thread.sleep(500);
-        }catch (Exception ex)
-        {
-            flag = false;
-        }
-        finally {
-            if(flag) System.out.println("Test completed successfully");
-            else System.out.println("test completed with error");
-        }
-   }
->>>>>>> 75a53b6062640e7266a3bdb27a706f955eb7b986
 
 
     @After
